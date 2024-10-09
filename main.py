6cd595d4e96e95980b5b385b014101ab9b8d9a1b
LenaKornichenko <lenakornicenko@gmail.com>
2024-10-09 22:57:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# практическая работа по теме динамическая типизация
+
+name = 'Lena'
+print ('Name:'+ name)
+age = 38
+print ('Age:', age)
+age = age+1
+print ('New age:' , age )
+is_student = age == age
+print ('Is_student:', is_student)
